b08dcdd97216cd533c1af7d88f212ee9b1cd99ec
Ryan <r.l.wells@outlook.com>
2019-06-02 13:04:37 -0500

    added form functionality

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h2>Create User</h2>
+
+    {!! Form::open(['method'=>'POST', 'action' => 'AdminUsersController@store']) !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name: ') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email: ') !!}
+        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role: ') !!}
+        {!! Form::select('role_id', ['' => 'Choose Options'] + $roles, null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('status', 'Status: ') !!}
+        {!! Form::select('status', array(1 => 'Active', 0 => 'Not Active'), null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">    
+        {!! Form::submit('Create User', ['class' => 'btn btn-primary']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+@stop
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <h2>Users</h2>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($users)
+        
+            @foreach($users as $user)
+
+            <tr>
+                <td>{{$user->id}}</td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->role->name}}</td>
+                <td>{{$user->created_at->diffForHumans()}}</td>
+                <td>{{$user->updated_at->diffForHumans()}}</td>
+                <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+            </tr>
+
+            @endforeach
+
+        @endif
+
+        </tbody>
+    </table>
+
+@stop
